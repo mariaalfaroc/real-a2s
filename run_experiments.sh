b1bf47682ffe7cacd06b5d73cf0e0a6f6b15651d
mariaalfaroc <mac77@alu.ua.es>
2024-02-15 14:49:27 +0000

    solved problems with madmom installation

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pip install Cython
+pip install madmom
 for i in {1..22}; do
     python train.py --experiment_id $i --encoding decoupled --batch_size 16 --epochs 300 --patience 300
 done
