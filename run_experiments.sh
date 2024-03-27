15586dfa4cc07ceb7773af17b916e7c4b0f8f984
María <56754030+mariaalfaroc@users.noreply.github.com>
2024-03-27 11:27:14 +0100

    Using decoupled_dot in final results

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,5 +3,5 @@
 pip install Cython
 pip install madmom
 for i in {1..22}; do
-    python train.py --experiment_id $i --encoding decoupled --batch_size 16 --epochs 300 --patience 300
+    python train.py --experiment_id $i --encoding decoupled_dot --batch_size 16 --epochs 300 --patience 300
 done
